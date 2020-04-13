4a632797457258a058920b9acd8d4016535ea20d
Peter <pdnguyen714@gmail.com>
2020-04-13 15:47:45 -0700

    Refactor avatar and skeleton

---
+++ project2-justiceHq/controllers/users.js
@@ -0,0 +1,43 @@
+const User = require('../models/user');
+
+module.exports = {
+  index,
+  addComment,
+  delComment
+};
+
+function index(req, res, next) {
+  console.log(req.query)
+  // Make the query object to use with Student.find based up
+  // the user has submitted the search form or now
+  let modelQuery = req.query.name ? {name: new RegExp(req.query.name, 'i')} : {};
+  // Default to sorting by name
+  let sortKey = req.query.sort || 'name';
+  User.find(modelQuery)
+  .sort(sortKey).exec(function(err, users) {
+    if (err) return next(err);
+    // Passing search values, name & sortKey, for use in the EJS
+    res.render('users/index', {
+      users,
+      user: req.user, // might need to change user to diff name
+      name: req.query.name,
+      sortKey
+    });
+  });
+}
+
+function addComment(req, res, next) {
+  req.user.facts.push(req.body);
+  req.user.save(function(err) {
+    res.redirect('/users');
+  });
+}
+
+function delComment(req, res, next) {
+  User.findOne({'facts._id': req.params.id}, function(err, user) {
+    user.facts.id(req.params.id).remove();
+    user.save(function(err) {
+      res.redirect('/users');
+    });
+  });
+}
