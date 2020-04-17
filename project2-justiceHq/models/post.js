9ed4911a6f24a50d344d91bbcc5ae98a65ada0ac
Peter <pdnguyen714@gmail.com>
2020-04-16 22:00:30 -0700

    Final Project

--- project2-justiceHq/models/post.js
+++ project2-justiceHq/models/post.js
@@ -1,28 +1,3 @@
-// const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
-
-
-// const commentSchema = new mongoose.Schema({
-//   text: String,
-//   user: {type: Schema.Types.ObjectId, ref: 'User'}
-// }, {
-//   timestamps: true
-// });
-
-// const postSchema = new mongoose.Schema({
-//   name: String,
-//   email: String,
-//   avatar: String,
-//   comment: [{type: Schema.Types.ObjectId, ref: 'Comment'}],
-//   googleId: String
-// }, {
-//   timestamps: true
-// });
-
-// module.exports = mongoose.model('Post', postSchema);
-
-
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -40,7 +15,7 @@ const postSchema = new mongoose.Schema({
     user: {
         type: Schema.Types.ObjectId,
         ref: 'User'},
-        comment: [{type: Schema.Types.ObjectId, ref: 'Comment'}],
+        comments: [commentSchema]
 },  
 
     { timestamps: true }
